89851c111d046a69f72e8a2d1b86d13fc5d098da
Daniela Vazquez <daniela_056@hotmail.com>
2020-07-12 23:09:19 -0600

    Finish JavaScript logic

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,16 @@ const difficultySelect = document.getElementById('difficulty');
 //*Fourth create function to add word to DOM
 //*Fifth add text.eventListener within this listener we will be using an updateScore function not yet created
 //*Sixth create updateScore function
+//*Seventh when page refreshes want cursor to automatically be in text area input field used text.focus();
+//*Eighth create const variable timeInterval that will allow us to run a function updateTime every 1000 miliseconds or 1s
+//*Ninth create updateTime function
+//*Tenth create gameOver function used in updateTime function
+//*Eleventh add time+= 5; to append time 
+//*Twelveth create settingsBtn and settingsForm eventListeners
+//*Thirteenth create global variable for difficulty level in local storage, then set difficulty select value
+//*Fourteenth need to implement difficulty level in typing eventListener because it is available in local storage but not yet available for use in app
+//*add if difficulty === hard then set time increment to 2... statement in eventListener Typing
+
 
 //List of words for game
 const words = [
@@ -56,6 +66,28 @@ let score = 0;
 //Init time
 let time = 10;
 
+//Set difficulty to value in local storage or medium
+//ternary operator for difficulty level vs. local storage
+//if difficulty !== not equal to null (if a difficulty is set to local storage) then set varible to localStorage getItem difficulty else set it to medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+
+
 //Generate random word from array
 //we have an arr called words and we need a random index or element from this arr
 //could also use an API with random words
@@ -77,10 +109,42 @@ function updateScore() {
   score++;
   scoreEl.innerHTML = score;
 }
+
+//Update time
+//every second or whatever is set in timeInterval local variable want time to count down
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  //if we don't add this timer will display negative seconds 
+  //use JS property called clearInterval and pass in variable timeInterval
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    //end game
+    gameOver();
+  }
+}
+
+//Game over, show end screen
+//added inline eventListener in button tag to listen for a click
+//in CSS we have endgameEl class end-game-container set to display none must change it to display flex
+function gameOver() {
+  endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p>Your final score is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+
 addWordToDOM();
 
 //Event Listeners
 
+//Typing
 //listens for a text input and listens for an event
 //will capture what user is typing and put it in a variable
 text.addEventListener('input', e => {
@@ -93,5 +157,26 @@ text.addEventListener('input', e => {
 
     //clear e.target.value set to empty string
     e.target.value = '';
+
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
+
+    updateTime();
   }
+});
+
+//Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+//Settings select
+//take our local storage object and call setItem which sets an item in local storage
+//we are going to call the item difficulty, and set it to that difficulty variable
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 });
